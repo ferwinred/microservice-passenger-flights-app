dc7005e6f947584e9f6d5308153ab2ef4f9308ae
Ferwin Arias <ferwin.arias@gmail.com>
2024-01-27 22:11:36 -0500

    Solving issue in the repository connection

--- src/passenger/passenger.module.ts
+++ src/passenger/passenger.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassengerService } from './application/passenger.service';
 import { PassengerController } from './infraestructure/passenger.controller';
+import { Passenger } from './domain/entities/passenger.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Passenger])
+  ],
   controllers: [PassengerController],
   providers: [PassengerService],
 })
